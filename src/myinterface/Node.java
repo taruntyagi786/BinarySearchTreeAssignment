912df32b07e3957366cfb9ed4c0b743d00b63157
taruntyagi786 <60462638+taruntyagi786@users.noreply.github.com>
2021-04-13 15:56:12 +0530

    Update Node.java

--- src/myinterface/Node.java
+++ src/myinterface/Node.java
@@ -1,5 +1,45 @@
 package myinterface;
 
 public interface Node<E extends Comparable<E>> {
-    //marker interface
+    myInterface.Node<E> {
+    private E data;
+    private Node<E> left,right;
+    public Node(E data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+
+    @Override
+    public E getData() {
+        return data;
+    }
+
+    @Override
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    @Override
+    public myInterface.Node<E> getLeft() {
+        return left;
+    }
+
+    @Override
+    public myInterface.Node<E> getRight() {
+        return right;
+    }
+
+    @Override
+    public void setLeft(myInterface.Node<E> left) {
+        this.left = (Node<E>)left;
+    }
+
+    @Override
+    public void setRight(myInterface.Node<E> right) {
+        this.right = (Node<E>) right;
+    }
+
+}
 }
